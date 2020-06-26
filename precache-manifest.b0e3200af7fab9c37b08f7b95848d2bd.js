2bc2f0b45973c8a5e0fbb78e7222b18bf52b8509
Sergii Goloborodko <sergei@kingmuffin.com>
2020-06-26 16:58:00 +0300

    Updates

---
+++ precache-manifest.b0e3200af7fab9c37b08f7b95848d2bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508aedea8c7378300ae3e5bbd8e82091",
+    "url": "/clients_list_app_demo/index.html"
+  },
+  {
+    "revision": "fffcc18671af540118fb",
+    "url": "/clients_list_app_demo/static/css/main.371634cb.chunk.css"
+  },
+  {
+    "revision": "ebe5cff5f64579cf5abb",
+    "url": "/clients_list_app_demo/static/js/2.d030fb69.chunk.js"
+  },
+  {
+    "revision": "d323d92aa6618b225df1a8e6c626da27",
+    "url": "/clients_list_app_demo/static/js/2.d030fb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffcc18671af540118fb",
+    "url": "/clients_list_app_demo/static/js/main.19883258.chunk.js"
+  },
+  {
+    "revision": "2ceaf68d83e4bb13c9ca",
+    "url": "/clients_list_app_demo/static/js/runtime-main.43b77e73.js"
+  },
+  {
+    "revision": "1ef7c992822656768e4e4dc276377302",
+    "url": "/clients_list_app_demo/static/media/Nobile-Regular.1ef7c992.ttf"
+  }
+]);
